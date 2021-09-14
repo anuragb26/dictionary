51916a017ec5165b4bfc1352bd83449d3a8a98b4
Anurag Bajaj <anuragb26@gmail.com>
2021-09-13 23:12:50 -0400

    Chore:Theming

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import { Container } from "@material-ui/core";
+import {
+  Container,
+  withStyles,
+  Switch,
+  createTheme,
+  Paper,
+} from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+import { ThemeProvider } from "@material-ui/core/styles";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
@@ -6,9 +14,27 @@ import Definitions from "./components/Definitions/Definitions";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const darkTheme = createTheme({
+    palette: { type: darkMode ? "dark" : "light", primary: { main: "#fff" } },
+  });
   const [meanings, setMeanings] = useState([]);
   const [category, setCategory] = useState("en");
   const [word, setWord] = useState("");
+  const DarkMode = withStyles({
+    switchBase: {
+      color: grey[300],
+      "&$checked": {
+        color: grey[500],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
   useEffect(() => {
     const dictionaryApi = async () => {
       try {
@@ -24,25 +50,51 @@ function App() {
     dictionaryApi();
   }, [category, word]);
   return (
-    <div
-      className="App"
-      style={{ height: "100vh", backgroundColor: "#282c34", color: "white" }}
-    >
-      <Container
-        maxWidth="md"
-        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
-      >
-        <Header
-          category={category}
-          setCategory={setCategory}
-          word={word}
-          setWord={setWord}
-        />
-        {meanings.length ? (
-          <Definitions word={word} meanings={meanings} category={category} />
-        ) : null}
-      </Container>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Paper>
+        <div
+          className="App"
+          style={{ height: "100vh", transition: "all 0.5s linear" }}
+        >
+          <Container
+            maxWidth="md"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100vh",
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                right: 15,
+                paddingTop: 10,
+              }}
+            >
+              <span>{darkMode ? "Dark" : "Light"} Mode</span>
+              <DarkMode
+                checked={darkMode}
+                onChange={() => setDarkMode(!darkMode)}
+              />
+            </div>
+            <Header
+              category={category}
+              setCategory={setCategory}
+              word={word}
+              setWord={setWord}
+            />
+            {meanings.length ? (
+              <Definitions
+                word={word}
+                meanings={meanings}
+                category={category}
+              />
+            ) : null}
+          </Container>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -2,44 +2,47 @@ import React from "react";
 import "./Definitions.css";
 
 const Definitions = ({ word, category, meanings }) => {
-  return (
-    <div className="meanings">
-      {meanings[0] && word && category === "en" ? (
-        <audio
-          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
-          controls
-          style={{ backgroundColor: "#fff", borderRadius: 10 }}
-        >
-          Your Browser does not support audio element
-        </audio>
-      ) : null}
-      {meanings.map((m) => {
-        return m.meanings.map((item) => {
-          return item.definitions.map((def) => (
-            <div
-              className="singleMean"
-              style={{ backgroundColor: "white", color: "black" }}
-            >
-              <b>{def.definition}</b>
-              <br />
-              {def.example && (
-                <span>
-                  <b>Example:{def.example}</b>
-                </span>
-              )}
-              <br />
-              {def.synonyms && (
-                <span>
-                  <b>Synonyms:{def.synonyms.map((s) => `${s},`)}</b>
-                </span>
-              )}
-              <hr style={{ backgroundColor: "white", width: "100%" }}></hr>
-            </div>
-          ));
-        });
-      })}
-    </div>
-  );
+  if (word) {
+    return (
+      <div className="meanings">
+        {meanings[0] && word && category === "en" ? (
+          <audio
+            src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+            controls
+            style={{ backgroundColor: "#fff", borderRadius: 10 }}
+          >
+            Your Browser does not support audio element
+          </audio>
+        ) : null}
+        {meanings.map((m) => {
+          return m.meanings.map((item) => {
+            return item.definitions.map((def) => (
+              <div
+                className="singleMean"
+                style={{ backgroundColor: "white", color: "black" }}
+              >
+                <b>{def.definition}</b>
+                <br />
+                {def.example && (
+                  <span>
+                    <b>Example:{def.example}</b>
+                  </span>
+                )}
+                <br />
+                {def.synonyms && (
+                  <span>
+                    <b>Synonyms:{def.synonyms.map((s) => `${s},`)}</b>
+                  </span>
+                )}
+                <hr style={{ backgroundColor: "white", width: "100%" }}></hr>
+              </div>
+            ));
+          });
+        })}
+      </div>
+    );
+  }
+  return null;
 };
 
 export default Definitions;
